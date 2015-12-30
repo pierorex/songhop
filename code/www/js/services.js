f85bd5cab519aed20e8436def8beac19abc76c20
Pierorex <jeanpierohm@gmail.com>
2015-12-30 10:25:11 +0000

    Added angular service called 'Recommendations', it connects to a remote server to get a random list of 10 songs and manages those songs inside the app. Implemented a way to cache the next song image so the app doesn't take too long to load it.

--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,25 +1,47 @@
-angular.module('songhop.services', [])
+angular.module('songhop.services', [])  /*global angular*/
 .factory('User', function() {
-
-  // array for our favorite songs
-  favorites = [];
   
-  addSongToFavorites = function(song) {
+  // array for our favorite songs
+  var o = {favorites: []};
+  
+  o.addSongToFavorites = function(song) {
     // make sure there's a song to add
     if (!song) return false;
 
     // add to favorites array
-    favorites.unshift(song);
+    o.favorites.unshift(song);
   };
   
-  removeSongFromFavorites = function(song, index) {
-    if (!song) return False;
-    favorites.splice(index, 1);
+  o.removeSongFromFavorites = function(song, index) {
+    if (!song) return false;
+    o.favorites.splice(index, 1);
   };
 
-  return {
-      favorites: favorites,
-      addSongToFavorites: addSongToFavorites,
-      removeSongFromFavorites: removeSongFromFavorites
+  return o;
+})
+
+.factory('Recommendations', function($http, SERVER) {
+  var o = {queue: []};
+  
+  o.getNextSongs = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations',
+    }).success(function (response) {
+      // add recomendations to queue
+      o.queue = o.queue.concat(response);
+    });
   };
+  
+  o.getNextSong = function() {
+    // pop first song
+    o.queue.shift();
+    
+    // low on queue? Let's fill it
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+  };
+  
+  return o;
 });
