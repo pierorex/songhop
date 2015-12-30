31f75238bc9384507310742d11ac7581bba9fb0d
Pierorex <jeanpierohm@gmail.com>
2015-12-30 09:36:42 +0000

    Added an angular service called User, it makes it possible to handle favorite songs (add and remove)

--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,1 +1,25 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+.factory('User', function() {
+
+  // array for our favorite songs
+  favorites = [];
+  
+  addSongToFavorites = function(song) {
+    // make sure there's a song to add
+    if (!song) return false;
+
+    // add to favorites array
+    favorites.unshift(song);
+  };
+  
+  removeSongFromFavorites = function(song, index) {
+    if (!song) return False;
+    favorites.splice(index, 1);
+  };
+
+  return {
+      favorites: favorites,
+      addSongToFavorites: addSongToFavorites,
+      removeSongFromFavorites: removeSongFromFavorites
+  };
+});
